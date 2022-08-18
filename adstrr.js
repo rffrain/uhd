f8a0386172449b77a1782ac2d897e9524385d4ea
rffrain <81103843+rffrain@users.noreply.github.com>
2022-08-18 23:16:55 +0800

    Create adstrr.js

---
+++ adstrr.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'cdbb9347424a33a49dbe5609090f8f55',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformats.com/cdbb9347424a33a49dbe5609090f8f55/invoke.js"></scr' + 'ipt>');
