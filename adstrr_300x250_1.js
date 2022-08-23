f5b653be9a57435be295fbe7b7f66cfc7abf6264
rffrain <81103843+rffrain@users.noreply.github.com>
2022-08-24 03:15:28 +0800

    Create adstrr_300x250_1.js

---
+++ adstrr_300x250_1.js
@@ -0,0 +1,1 @@
+atOptions={key:"cdbb9347424a33a49dbe5609090f8f55",format:"iframe",height:250,width:300,params:{}},document.write('<script type="text/javascript" src="http'+("https:"===location.protocol?"s":"")+'://www.effectivecreativeformats.com/cdbb9347424a33a49dbe5609090f8f55/invoke.js"><\/script>');
