c5605507341c32ae767c034cd42421b358fddc31
rffrain <81103843+rffrain@users.noreply.github.com>
2022-08-28 20:43:25 +0800

    Update adstrr_bar.js

--- adstrr_bar.js
+++ adstrr_bar.js
@@ -1,1 +1,27 @@
-if (window.innerWidth>728){atOptions={key:"d6f9d6f6bb876c34adf7732505afa231",format:"iframe",height:90,width:728,params:{}},document.write('<script type="text/javascript" src="http'+("https:"===location.protocol?"s":"")+'://www.effectivecreativeformats.com/d6f9d6f6bb876c34adf7732505afa231/invoke.js"><\/script>');}else{atOptions={key:"dd396fcf945860975f6c75cacf6173e4",format:"iframe",height:50,width:320,params:{}},document.write('<script type="text/javascript" src="http'+("https:"===location.protocol?"s":"")+'://www.effectivecreativeformats.com/dd396fcf945860975f6c75cacf6173e4/invoke.js"></script>')}
+if (window.innerWidth>728){atOptions={key:"d6f9d6f6bb876c34adf7732505afa231",format:"iframe",height:90,width:728,params:{}},document.write('<script type="text/javascript" src="http'+("https:"===location.protocol?"s":"")+'://www.effectivecreativeformats.com/d6f9d6f6bb876c34adf7732505afa231/invoke.js"><\/script>');
+
+function fx0() {
+  document.getElementById("stcky").style.marginBottom = "-115px";
+  document.getElementById("x_1").style.display = "block";
+  document.getElementById("x_0").style.display = "none";
+}
+function fx1() {
+  document.getElementById("stcky").style.marginBottom = "";
+  document.getElementById("x_1").style.display = "none";
+  document.getElementById("x_0").style.display = "block";
+}
+                          
+}else{
+ atOptions={key:"dd396fcf945860975f6c75cacf6173e4",format:"iframe",height:50,width:320,params:{}},document.write('<script type="text/javascript" src="http'+("https:"===location.protocol?"s":"")+'://www.effectivecreativeformats.com/dd396fcf945860975f6c75cacf6173e4/invoke.js"></script>')
+
+function fx0() {
+  document.getElementById("stcky").style.marginBottom = "-65px";
+  document.getElementById("x_1").style.display = "block";
+  document.getElementById("x_0").style.display = "none";
+}
+function fx1() {
+  document.getElementById("stcky").style.marginBottom = "";
+  document.getElementById("x_1").style.display = "none";
+  document.getElementById("x_0").style.display = "block";
+}
+}
